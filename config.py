13114d9d5fe2a952426ec3e00e8dbd0c74b51302
Jordan Yee <11jor@Jordans-Computer.local>
2026-01-11 16:40:58 -0500

    Added pieces to board

---
+++ config.py
@@ -0,0 +1,24 @@
+
+
+WHITE = "w"
+BLAKC = "b"
+
+UNICODE_PIECES = {
+    "K": "♔",
+    "Q": "♕",
+    "R": "♖",
+    "B": "♗",
+    "N": "♘",
+    "P": "♙",
+    "k": "♚",
+    "q": "♛",
+    "r": "♜",
+    "b": "♝",
+    "n": "♞",
+    "p": "♟",
+}
+
+BOARDSIZE = 800
+SQUARESIZE = BOARDSIZE/8
+FPS = 60
+
